6dd049ac1875946d5a9d4ea9c3055bbe6ffcbc98
Garrick Hutcheson <garrick.hutcheson@gmail.com>
2018-04-16 12:23:50 -0700

    formatting

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -5,15 +5,13 @@
 
 #include "Scheduler.h"
 
-
-
 Scheduler::Scheduler() {
   allTheProcs = 13;
   avaliableProcs = allTheProcs;
   totalJobs = 0; // starts at the first line of a file
   fin.open("Input.txt");
   exitScheduler = false;
-  inputMode=0;
+  inputMode = 0;
 }
 Scheduler::Scheduler(int mode, int procs) {
   avaliableProcs = procs;
@@ -21,36 +19,36 @@ Scheduler::Scheduler(int mode, int procs) {
   totalJobs = 0;
   fin.open("Input.txt");
   exitScheduler = false;
-  inputMode=mode;
+  inputMode = mode;
 }
 
-//runs jobs from file w/o user input
+// runs jobs from file w/o user input
 void Scheduler::Run() {
-  do {  
+  do {
     Tick();
-    if(exitScheduler){
-      std::cout << "exiting" <<std::endl;
+    if (exitScheduler) {
+      std::cout << "exiting" << std::endl;
       return;
     }
   } while (!(procaQueue.top().getDesc() == "exit") || !running.empty());
 }
 
 void Scheduler::waitForUserInput() {
-  string jobdesk="";
-  int numprocs=0;
-  int numticks=0;
+  string jobdesk = "";
+  int numprocs = 0;
+  int numticks = 0;
   std::cout << "Enter your job description or type exit to exit: " << std::endl;
   std::cin >> jobdesk;
-  if(!jobdesk.compare("exit")){
-      exitScheduler = true;
+  if (!jobdesk.compare("exit")) {
+    exitScheduler = true;
   } else {
     std::cout << "Enter required number of processors " << std::endl;
     std::cin >> numprocs;
-    std::cout << "Enter required number of ticks " << std::endl;  
+    std::cout << "Enter required number of ticks " << std::endl;
     std::cin >> numticks;
-    if(numprocs !=0){
+    if (numprocs != 0) {
       int id = totalJobs;
-      insertJob(totalJobs,numprocs,numticks,jobdesk);
+      insertJob(totalJobs, numprocs, numticks, jobdesk);
     }
   }
 }
@@ -61,26 +59,24 @@ void Scheduler::waitForUserInput() {
   prints the job_id of any jobs compleated during the tick
 */
 void Scheduler::Tick() {
-  std::cout<<"\nTICK\n";
-  if(inputMode==0){
+  std::cout << "\nTICK\n";
+  if (inputMode == 0) {
     if (!fin.eof()) {
       exitScheduler = getAJobFromTextFile();
     } else {
-      insertJob(-1,allTheProcs,0,"exit");
+      insertJob(-1, allTheProcs, 0, "exit");
     }
-  }else{
+  } else {
     waitForUserInput();
   }
- if(!procaQueue.empty() || !exitScheduler)
- {
-  scheduleJob();
- }
- if(!running.empty() || !exitScheduler)
- {
-  deleteByTimer();
- }
-}
+  if (!running.empty() || !exitScheduler) {
+    deleteByTimer();
+  }
 
+  if (!procaQueue.empty() || !exitScheduler) {
+    scheduleJob();
+  }
+}
 /*
   insertJob
   adds a job to the priority queue
@@ -93,9 +89,9 @@ void Scheduler::insertJob(int id, int procs, int ticks, string desc) {
   if ((0 <= procs) && (0 <= ticks)) {
     Job toPriorityQueue = Job(id, procs, ticks, desc);
     procaQueue.push(toPriorityQueue);
-    if(id!=-1)
-      std::cout << toPriorityQueue << " inserted to priority Queue" << std::endl;
-
+    if (id != -1)
+      std::cout << toPriorityQueue << " inserted to priority Queue"
+                << std::endl;
   }
 }
 
@@ -129,16 +125,14 @@ bool Scheduler::checkAvailiability(int procs) {
 */
 void Scheduler::scheduleJob() {
   int procs = procaQueue.top().getProcs();
-  if(checkAvailiability(procs)&&(procaQueue.top().getID()!=-1))
-  {
-    avaliableProcs-= procs;
+  if (checkAvailiability(procs) && (procaQueue.top().getID() != -1)) {
+    avaliableProcs -= procs;
     Job toRunning = deleteShortest();
     running.push_back(toRunning);
 
-    std::cout << toRunning <<
-    " added to running processes and removed from priority queue"
-    << std::endl;
-
+    std::cout << toRunning
+              << " added to running processes and removed from priority queue"
+              << std::endl;
   }
 }
 
@@ -147,9 +141,7 @@ void Scheduler::scheduleJob() {
   adds procs back to pool checks to
 */
 void Scheduler::releaseProcs(int procs) {
-    {
-       (avaliableProcs += procs);
-    }
+  { (avaliableProcs += procs); }
 }
 
 /*
@@ -159,14 +151,13 @@ void Scheduler::releaseProcs(int procs) {
 */
 bool Scheduler::getAJobFromTextFile() {
   string desc;
-  int procs = 0;    
+  int procs = 0;
   int ticks = 0;
   getline(fin, desc, ' ');
-  for(int i = 0; i < desc.length(); i++)
-  {
+  for (int i = 0; i < desc.length(); i++) {
     desc[i] = tolower(desc[i]);
   }
-  if(desc.find("exit") == string::npos) {
+  if (desc.find("exit") == string::npos) {
     fin >> procs;
     fin >> ticks;
     fin.ignore(1000, '\n');
@@ -174,30 +165,28 @@ bool Scheduler::getAJobFromTextFile() {
     return false;
   }
   return true;
-  
 }
-
-
 
 /*prints the job_ids of any jobs compleated during the tick*/
 void Scheduler::deleteByTimer() {
   int syntactorator = 0;
-    for (std::list<Job>::iterator iter = running.begin(); iter != running.end();iter++) {
-      iter->decrementTimer();    
-      if ((iter->getTimer() == 0)) {
-        std::list<Job>::iterator deleteIter = iter; //don't erase loop guard
-        std::cout << *iter <<", ";
-        releaseProcs(iter->getProcs());
-        iter--; //step off to be erased node
-        running.erase(deleteIter); //erase node
-        syntactorator++;
-      }
+  for (std::list<Job>::iterator iter = running.begin(); iter != running.end();
+       iter++) {
+    iter->decrementTimer();
+    if ((iter->getTimer() == 0)) {
+      std::list<Job>::iterator deleteIter = iter; // don't erase loop guard
+      std::cout << *iter << ", ";
+      releaseProcs(iter->getProcs());
+      iter--;                    // step off to be erased node
+      running.erase(deleteIter); // erase node
+      syntactorator++;
     }
-    std::cout << extraSyntax(syntactorator);
+  }
+  std::cout << extraSyntax(syntactorator);
 }
 
 string Scheduler::extraSyntax(int syntactor) {
-  return ((syntactor) ? ((syntactor - 1) ? ("were finished.\n")
-                                                  : ("was finished.\n"))
-                          : ("No jobs were finished\n"));
+  return ((syntactor)
+              ? ((syntactor - 1) ? ("were finished.\n") : ("was finished.\n"))
+              : ("No jobs were finished\n"));
 }
